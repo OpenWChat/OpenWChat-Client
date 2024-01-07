65a602cfe90f7c595712c4681d3f2bcb1a9d6200
AliSabet81 <sabet.farshid1381@gmail.com>
2024-01-07 11:13:34 +0330

    feat(component): implement routes and pages co-authored-by: mahdi-eth <mahdi.ettehad85@gmail.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,21 +2,40 @@
 import { createBrowserRouter } from "react-router-dom";
 import React, { Suspense } from "react";
 
+// Pages
 const LoginPage = React.lazy(() => import("../Pages/login"));
+const HomePage = React.lazy(() => import("../Pages/home"));
+const RegisterPage = React.lazy(() => import("../Pages/register"));
 
+// Routes
 export const ROUTES = {
-  auth: {
-    login: "/",
-    otp: "/otp",
-  },
+  home: "/",
+  login: "/login",
+  register: "/register",
 };
 
 export const router = createBrowserRouter([
   {
-    path: ROUTES.auth.login,
+    path: ROUTES.home,
+    element: (
+      <Suspense>
+        <HomePage />
+      </Suspense>
+    ),
+  },
+  {
+    path: ROUTES.login,
     element: (
       <Suspense>
         <LoginPage />
+      </Suspense>
+    ),
+  },
+  {
+    path: ROUTES.register,
+    element: (
+      <Suspense>
+        <RegisterPage />
       </Suspense>
     ),
   },
