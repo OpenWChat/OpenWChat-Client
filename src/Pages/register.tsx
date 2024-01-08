47e77507f2a8bb22f0a38dacc341413a7dd02610
AliSabet81 <sabet.farshid1381@gmail.com>
2024-01-08 08:34:03 +0330

    starting login page
    co-authored-by: Co-Author Name <mahdi.ettehad85@gmail.com>

--- src/Pages/register.tsx
+++ src/Pages/register.tsx
@@ -1,9 +1,10 @@
 const Register = () => {
   return (
     <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
-        <div className="flex w-[1600px] mx-auto h-full">
-            
-        </div>
+      {/* Container */}
+      <div className="flex w-[1600px] mx-auto h-full">
+        {/* Register Form */}
+      </div>
     </div>
   );
 };
