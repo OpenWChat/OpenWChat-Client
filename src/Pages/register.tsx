ceed5650bcbd7d0b5a509ddde240e8a9d8f0fb56
AliSabet81 <sabet.farshid1381@gmail.com>
2024-01-08 08:52:56 +0330

    starting register form
    co-authored-by: Co-Author Name <mahdi.ettehad85@gmail.com>

--- src/Pages/register.tsx
+++ src/Pages/register.tsx
@@ -1,9 +1,12 @@
+import { RegisterForm } from "@/components";
+
 const Register = () => {
   return (
     <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
       {/* Container */}
       <div className="flex w-[1600px] mx-auto h-full">
         {/* Register Form */}
+        <RegisterForm />
       </div>
     </div>
   );
