b179992127c7e6092bd80388d714192d0f2f4c5b
AliSabet81 <sabet.farshid1381@gmail.com>
2024-01-10 10:24:28 +0330

    register full schema
    co-authored-by: Co-Author Name <mahdi.ettehad85@gmail.com>

--- src/components/auth/RegisterForm/RegisterForm.tsx
+++ src/components/auth/RegisterForm/RegisterForm.tsx
@@ -3,21 +3,25 @@ import { signUpSchema } from "@/utils";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import { AuthInput } from "@/components";
+import { useDispatch, useSelector } from "react-redux";
+import PulseLoader from "react-spinners/PulseLoader";
+import { ROUTES } from "@/routes";
+import { Link } from "react-router-dom";
+import { registerUser } from "@/features";
 
 export const RegisterForm = () => {
+  const dispatch: any = useDispatch();
+  const { status, error } = useSelector((status: any) => status.user);
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(signUpSchema),
   });
   const handleRegisterSubmit = (data: any) => {
-    console.log(data);
+    dispatch(registerUser({ data, picture: "" }));
   };
-  console.log("values", watch());
-  console.log("errors", errors);
   return (
     <div className="h-screen w-full flex items-center justify-center overflow-hidden">
       {/* Container */}
@@ -60,7 +64,49 @@ export const RegisterForm = () => {
             register={register}
             error={errors?.password?.message}
           />
-          <button type="submit">submit</button>
+          {/* if we have an error */}
+          {error ? (
+            <div>
+              <p className="text-red-400">{error}</p>
+            </div>
+          ) : null}
+          {/* Submit button */}
+          <button
+            className="
+            w-full 
+            flex 
+            justify-center 
+            bg-green_1 
+            text-gray-100 
+            p-4 
+            rounded-full 
+            tracking-wide
+            font-semibold 
+            focus:outline-none 
+            hover:bg-green_2 
+            shadow-lg 
+            cursor-pointer 
+            transition 
+            ease-in 
+            duration-300"
+            type="submit"
+          >
+            {status === "loading" ? (
+              <PulseLoader color="#fff" size={16} />
+            ) : (
+              "Sign Up"
+            )}
+          </button>
+          {/* Sign in Link */}
+          <p className="flex flex-col items-center justify-center mt-10 text-center text-md dark:text-dark_text_1">
+            <span>have an account ?</span>
+            <Link
+              className="hover:underline cursor-pointer transition ease-in duration-300"
+              to={ROUTES.login}
+            >
+              Sign in
+            </Link>
+          </p>
         </form>
       </div>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  server:{
-    host:true,
-    port:3333
+  server: {
+    host: true,
+    port: 3333,
   },
   preview: {
     port: 3333,
     strictPort: true,
-    host:true
+    host: true,
   },
-  plugins: [react(),tsconfigPaths()],
-})
+  define: {
+    "process.env": {},
+  },
+  plugins: [react(), tsconfigPaths()],
+});
