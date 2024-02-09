e0264e464cb8633bd8956b0fefeea30aeb558735
AliSabet81 <sabet.farshid1381@gmail.com>
2024-02-09 21:34:44 +0330

    starting home page

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -1,5 +1,15 @@
+import { Sidebar } from "@/components";
+
 const Home = () => {
-    return ( <div>Home</div> );
-}
- 
+  return (
+    <div className="min-h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
+      {/* Container */}
+      <div className="container min-h-screen flex">
+        {/* Sidebar */}
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
 export default Home;
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,10 @@
+import { SideBarHeader } from "./header";
+
+export const Sidebar = () => {
+  return (
+    <div className="w-[40%] h-full select-none">
+      {/* Sidebar Header */}
+      <SideBarHeader />
+    </div>
+  );
+};
