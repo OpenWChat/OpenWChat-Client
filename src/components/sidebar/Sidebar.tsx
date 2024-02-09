87f0fa782b7e7b5df15b08bcf19b86571605f851
AliSabet81 <sabet.farshid1381@gmail.com>
2024-02-09 21:51:08 +0330

    starting side bar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,10 +1,13 @@
 import { SideBarHeader } from "./header";
+import { Notifications } from "./notifications";
 
 export const Sidebar = () => {
   return (
     <div className="w-[40%] h-full select-none">
       {/* Sidebar Header */}
       <SideBarHeader />
+      {/* Notifications */}
+      <Notifications />
     </div>
   );
 };
