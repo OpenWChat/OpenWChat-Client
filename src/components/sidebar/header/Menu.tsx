ca5b342cc4c8690dc1ba7922b83e6db6f179585b
AliSabet81 <sabet.farshid1381@gmail.com>
2024-02-12 09:39:45 +0330

    side bar menu logout logic

--- src/components/sidebar/header/Menu.tsx
+++ src/components/sidebar/header/Menu.tsx
@@ -1,4 +1,8 @@
+import { logout } from "@/features";
+import { useDispatch } from "react-redux";
+
 export const Menu = () => {
+  const dispatch = useDispatch();
   return (
     <div className="absolute right-1 z-50 dark:bg-dark_bg_2 dark:text-dark_text_1 shadow-md w-52">
       <ul>
@@ -14,7 +18,10 @@ export const Menu = () => {
         <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
           <span>Setting</span>
         </li>{" "}
-        <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
+        <li
+          className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3"
+          onClick={() => dispatch(logout())}
+        >
           <span>Logout</span>
         </li>{" "}
       </ul>
